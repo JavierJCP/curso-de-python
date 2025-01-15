c5db5074b51de6a4e6c057769aead956e42f0ae5
Javier Casafranca <@JavierJCP>
2025-01-15 15:07:17 -0500

    add print section

---
+++ clase1/01_print.py
@@ -0,0 +1,10 @@
+print("Hola mundo")
+
+print('Tambien funciona con comillas simples')
+
+print('Python', "es", 'genial')
+
+print("Python", "es", "genial", sep=" - ")
+
+print("es se imprime", end=" ")
+print("en una sola linea")
---
+++ clase1/variables.py
@@ -0,0 +1,2 @@
+name= "javier"
+print(name)
