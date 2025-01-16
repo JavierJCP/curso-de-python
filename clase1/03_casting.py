55927f40cd3c4e9439392268982f2c294e4e01fa
Javier Casafranca <@JavierJCP>
2025-01-15 20:54:38 -0500

    add variables section

--- clase1/03_casting.py
+++ clase1/03_casting.py
@@ -11,9 +11,10 @@ print(type(int("10")))
 print("100" + str(2)) # concatenacion
 
 print(float("3.141516"))
-print(int(3.141516))
+print(int(3.141516)) # no hace un redondeo, solo elimina la parte decimal
+print(round(1.5))  # redondea al par entero mas cercano
 
 print(bool(-1))
 print(bool(0)) # unico valor que devuelve False
 print(bool("False"))
-print(bool(""))
+print(bool("")) # unico valor str que devuelve False
