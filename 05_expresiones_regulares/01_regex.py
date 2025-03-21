5ab3ffc3eb6e3b1a9be968080d1c35292392b35d
Javier Casafranca <@JavierJCP>
2025-03-21 09:50:27 -0500

    add regex re

--- 05_expresiones_regulares/01_regex.py
+++ 05_expresiones_regulares/01_regex.py
@@ -2,8 +2,10 @@
 # 01 - Expresiones regulares
 # 
 
-""" Las expresiones regulares son una secuencia de caracteres que forman un patrón de búsqueda.
-    Se utilizan para la búsqueda de cadenas de texto, validación de datos, etc. """
+""" 
+  Las expresiones regulares son una secuencia de caracteres que forman un patrón de búsqueda.
+  Se utilizan para la búsqueda de cadenas de texto, validación de datos, etc.
+"""
 
 
 """ ¿Por qué aprender Regex?
@@ -24,10 +26,8 @@ text = "Hola mundo"
 # 4. Usar la función de búsqueda de "re"
 result = re.search(pattern, text)
 
-if result:
-  print("He encontrado el patrón en el texto")
-else:
-  print("No he encontrado el patrón en el texto")
+if result: print("He encontrado el patrón en el texto")
+else: print("No he encontrado el patrón en el texto")
 
 # .group() devuelve la cadena que coincide con el pattern
 print(result.group())
@@ -41,6 +41,7 @@ print(result.end())
 # EJERCICIO 01
 # Encuentra la primera ocurrencia de la palabra "IA" en el siguiente texto
 # e indica en que posición empieza y termina la coincidencia.
+
 text = "Todo el mundo dice que la IA nos va a quitar el trabajo. Pero solo hace falta ver cómo la puede cagar con las Regex para ir con cuidado"
 pattern = "IA"
 found_ia = re.search(pattern, text)
@@ -59,15 +60,15 @@ text = "Me gusta Python. Python es lo máximo. Aunque Python no es tan difícil,
 pattern = "Python"
 
 matches = re.findall(pattern, text)
-
+# print(matches)
 print(len(matches))
 
 # -------------------------
 
 # iter() devuelve un iterador que contiene todos los resultados de la búsqueda
 
-text = "Me gusta Python. Python es lo máximo. Aunque Python no es tan difícil, ojo con Python"
-pattern = "Python"
+text = "Me gusta Python. Pyhhon es lo máximo. Aunque Python no es tan difícil, ojo con Python"
+pattern = "Py.hon" # el punto indica que puede ser cualquier caracter
 
 matches = re.finditer(pattern, text)
 
@@ -77,6 +78,12 @@ for match in matches:
 # EJERCICIO 02
 # Encuentra todas las ocurrencias de la palabra "midu" en el siguiente texto e indica en que posición empieza y termina cada coincidencia y cuantas veces se encontró.
 text = "Este es el curso de Python de midudev. ¡Suscríbete a midudev si te gusta este contenido! midu"
+pattern = "midu."
+matches = re.finditer(pattern, text)
+times = len(re.findall(pattern, text))
+for match in matches:
+  print(match.group(), match.start(), match.end())
+print(f"He encontrado {times} coincidencias")
 
 ### Modificadores
 
@@ -88,11 +95,15 @@ text = "Todo el mundo dice que la IA nos va a quitar el trabajo. Pero la ia no e
 pattern = "IA"
 found = re.findall(pattern, text, re.IGNORECASE)
 
-if found: print(found)
+if found: 
+  print(found)
 
 # EJERCICIO 03
 # Encuentra todas las ocurrencias de la palabra "python" en el siguiente texto, sin distinguir entre mayúsculas y minúsculas.
 text = "Este es el curso de Python de midudev. ¡Suscríbete a python si te gusta este contenido! PYTHON"
+pattern="python"
+matches = re.findall(pattern, text, re.IGNORECASE)
+print(matches)
 
 ### Reemplazar el texto
 
@@ -104,3 +115,6 @@ replacement = "Adiós"
 
 new_text = re.sub(pattern, replacement, text, flags=re.IGNORECASE)
 print(new_text)
+
+# new_text = re.sub(pattern, replacement, text, count=2,flags=re.IGNORECASE)
+# print(new_text)
